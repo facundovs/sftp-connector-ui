d19638723fcaf01f54950dcd0562f2f692455a44
Alvaro Durandal <adurandal@elementum.com>
2018-06-15 15:23:35 -0300

    adding select entityType event

--- app/scripts/controllers/data-mapper.js
+++ app/scripts/controllers/data-mapper.js
@@ -6,10 +6,19 @@ angular.module('sftpApp')
     console.info("Data Mapper controller is starting...");
 
     var vm = this;
+    vm.mapper = {};
+    vm.showElementumTemplate = false;
 
     vm.getSuggestions = function () {
       console.info("Getting suggestions...");
       $location.path( "suggestion" );
     };
+
+    vm.changeEntityType = function() {
+      console.info("change Entity type:" + vm.mapper.entityType);
+      vm.showElementumTemplate = vm.mapper.entityType != '';
+    }
+
+
 
   });
